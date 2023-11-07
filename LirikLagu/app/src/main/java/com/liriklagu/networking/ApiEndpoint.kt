5c6349b80a4668a7747d574dd9bc2edc59cf5880
Sachita <99628530+0100010110@users.noreply.github.com>
2023-11-07 12:12:42 +0700

    Add files via upload

---
+++ LirikLagu/app/src/main/java/com/liriklagu/networking/ApiEndpoint.kt
@@ -0,0 +1,10 @@
+package com.liriklagu.networking
+
+object ApiEndpoint {
+
+    var BASEURl = "http://song-lyrics-api.azharimm.dev/hot"
+
+    var SEARCHURl = "http://song-lyrics-api.azharimm.dev/search?q={query}"
+
+    var DETAILURl = "http://song-lyrics-api.azharimm.dev/lyrics/{id}"
+}
---
+++ LirikLagu/app/src/main/java/com/liriklagu/utils/OnItemClickCallback.kt
@@ -0,0 +1,7 @@
+package com.liriklagu.utils
+
+import com.azhar.mylyrics.model.ModelMain
+
+interface OnItemClickCallback {
+    fun onItemClicked(modelMain: ModelMain)
+}

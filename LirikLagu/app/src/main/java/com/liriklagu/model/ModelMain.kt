a6639bbd19e81d61810542c8f85ebc5889dabe55
Sachita <99628530+0100010110@users.noreply.github.com>
2023-11-28 10:52:32 +0700

    Delete LirikLagu directory

--- LirikLagu/app/src/main/java/com/liriklagu/model/ModelMain.kt
+++
@@ -1,9 +0,0 @@
-package com.liriklagu.model
-
-import java.io.Serializable
-
-class ModelMain : Serializable {
-    var strId: String = ""
-    var strArtis: String = ""
-    var strTitle: String = ""
-}
--- LirikLagu/app/src/main/java/com/liriklagu/utils/OnItemClickCallback.kt
+++
@@ -1,7 +0,0 @@
-package com.liriklagu.utils
-
-import com.azhar.mylyrics.model.ModelMain
-
-interface OnItemClickCallback {
-    fun onItemClicked(modelMain: ModelMain)
-}
